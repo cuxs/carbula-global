1b30b030218ca43dfcc0294b6d0602650d081bc9
Mariano <riano.qx@gmail.com>
2021-12-13 20:48:26 -0300

    Remove redirects

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   i18n,
-  async redirects(){
-    return[
-      {
-        source: 'www.carbula.com.mx',
-        destination: 'www.carbula.mx',
-        permanent: true,
-      },
-      {
-        source: 'www.carbula.com.uy',
-        destination: 'www.carbula.uy',
-        permanent: true,
-      }
-    ]
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.

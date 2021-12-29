9f6a3c9c83840f8e5cc2d54bc6fded8690d61257
Mariano <riano.qx@gmail.com>
2021-12-29 19:06:44 -0300

    Fix mex phone format

--- components/SellForm/index.jsx
+++ components/SellForm/index.jsx
@@ -227,7 +227,7 @@ const SellForm = ({ step, setStep, setOverlayBackground, zonas, referer, COUNTRY
     const masks = {
       'ar': '+54 9 nnn nnnn nnnn',
       'uy': '+598 nnnn nnnn',
-      'mx': '+52 nn nnnn nnnn',
+      'mx': '+52 nnn nnn nnnn',
       'cl': '+56 9 nnnn nnnn'
     }
     return masks[COUNTRY_CODE]
--- pages/cotizacion/index.jsx
+++ pages/cotizacion/index.jsx
@@ -181,8 +181,8 @@ const Cotizacion = ({COUNTRY_CODE}) => {
               <h3>para su {capitalize(cotizationData.brand)} {capitalize(cotizationData.model)} {capitalize(cotizationData.year)}</h3>
             </animated.div>
             <animated.div style={priceProps} className={styles.price__comparison}>
-              {cotizationData.retake_price && <div className={styles.automotora__price}><h4 className={styles.red}>{t('priceComparison.unaConsesionaria')}</h4><h3>{CURRENCY[COUNTRY_CODE]}${formatNumber(cotizationData.retake_price, 0)}</h3></div>}
-              <div className={styles.carbula__price}><h4>{t('priceComparison.withCarbula')}</h4><h3>{CURRENCY[COUNTRY_CODE]}${formatNumber(selectedPrice, 0)}</h3></div>
+              {cotizationData.retake_price && <div className={styles.automotora__price}><h4 className={styles.red}>{t('priceComparison.unaConsesionaria')}</h4><h3>{CURRENCY[COUNTRY_CODE]}$ {formatNumber(cotizationData.retake_price, 0)}</h3></div>}
+              <div className={styles.carbula__price}><h4>{t('priceComparison.withCarbula')}</h4><h3>{CURRENCY[COUNTRY_CODE]}$ {formatNumber(selectedPrice, 0)}</h3></div>
             </animated.div>
             <animated.div style={faqProps}>
               {width > 769 && <FaqCotization />}

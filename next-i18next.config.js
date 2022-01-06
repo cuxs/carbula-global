f1599f01bdae295106c0877401c75629765b6d66
Mariano <riano.qx@gmail.com>
2022-01-06 14:23:45 -0300

    Disable locale detection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
+    localeDetection:false,
     locales: ['es-AR', 'es-UY', 'es-MX', 'es-CL'],
     defaultLocale: 'es-AR',
     localePath: path.resolve('./public/locales'),

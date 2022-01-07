b5af8bc158df75ace578b238e36cf9699da46e29
Mariano <riano.qx@gmail.com>
2022-01-07 17:22:55 -0300

    Add country redirects

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,14 +2,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const { geo, url} = req
-  const country = geo.country || 'US'
+  const { geo, nextUrl} = req
 
-  console.log('url', url)
-  console.log('geo', geo)
-  if(geo.country === "AR"){
-    return NextResponse.rewrite('https://google.com')
+  const topLevelDomain = nextUrl.origin.split('.')[1]
+  if(topLevelDomain !== geo.country.toLowerCase()){
+    if(geo.country === "AR"){
+      return NextResponse.rewrite('https://carbula.com')
+    }
+    if(geo.country === "CL"){
+      return NextResponse.rewrite('https://carbula.cl')
+    }
+    if(geo.country === "MX"){
+      return NextResponse.rewrite('https://carbula.mx')
+    }
+    if(geo.country === "UY"){
+      return NextResponse.rewrite('https://carbula.uy')
+    }
   }
-
-  // return NextResponse.rewrite()
 }

1737bd24bc7c7eecf81f67e4f1eb3d791b0cd413
Mariano <mariano.fliri@gmail.com>
2022-01-09 09:29:03 -0300

    do not redirect .com if country = ar

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,11 +4,8 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function middleware(req) {
   const { geo, nextUrl} = req
 
-  const topLevelDomain = nextUrl.origin.split('.')[1]
+  const topLevelDomain = nextUrl.origin.split('.').pop()
   if(topLevelDomain !== geo.country.toLowerCase()){
-    if(geo.country === "AR"){
-      return NextResponse.rewrite('https://carbula.com')
-    }
     if(geo.country === "CL"){
       return NextResponse.rewrite('https://carbula.cl')
     }

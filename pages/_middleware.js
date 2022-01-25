5fadd890767e038bc0a6185958ffb4562d5d0184
Mariano <riano.qx@gmail.com>
2022-01-25 19:14:27 -0300

    Only use middleware in production

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,21 +2,25 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const { geo, nextUrl} = req
+  if (process.env.NODE_ENV !== 'development') {
 
-  const topLevelDomain = nextUrl.origin.split('.').pop()
-  if(topLevelDomain !== geo.country.toLowerCase()){
-    if(geo.country === "AR" && topLevelDomain != "com"){
-      return NextResponse.rewrite("https://carbula.com")
-    }
-    if(geo.country === "CL"){
-      return NextResponse.rewrite('https://carbula.cl')
-    }
-    if(geo.country === "MX"){
-      return NextResponse.rewrite('https://carbula.mx')
-    }
-    if(geo.country === "UY"){
-      return NextResponse.rewrite('https://carbula.uy')
+    const { geo, nextUrl } = req
+
+    const topLevelDomain = nextUrl.origin.split('.').pop()
+    if (topLevelDomain !== geo.country.toLowerCase()) {
+      if (geo.country === "AR" && topLevelDomain != "com") {
+        return NextResponse.rewrite("https://carbula.com")
+      }
+      if (geo.country === "CL") {
+        return NextResponse.rewrite('https://carbula.cl')
+      }
+      if (geo.country === "MX") {
+        return NextResponse.rewrite('https://carbula.mx')
+      }
+      if (geo.country === "UY") {
+        return NextResponse.rewrite('https://carbula.uy')
+      }
     }
   }
+
 }

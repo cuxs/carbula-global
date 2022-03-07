792cc1d0995123b1f1940e91cd41d1d4ac3e6688
Desarrollo <jbmg21@protonmail.com>
2022-03-07 14:38:40 -0300

    Changed FB domain verification code

--- utils/helpers.js
+++ utils/helpers.js
@@ -429,7 +429,7 @@ export const getKeywordsByCountry = (country_code) => {
 
 export const getFacebookDomainVerification = (country_code) => {
   const ids = {
-    ar: 'o1cpz3juti6u4og457vtz73phm8v4m',
+    ar: 'brd5b5y4sra61yxq5iecd6omyq4sis',
     cl: 'xttlstugadcn3syxvsdpcr2kpm0ajs',
     mx: 'xbbkq67yvogzf0656jxyzlwamjm4tp',
     uy: 'u9xibrmbxi98j13w4lv7b1baytrgvy',

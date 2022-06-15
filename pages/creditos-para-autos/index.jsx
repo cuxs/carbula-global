288758cfd4f7afb7aa3de965585359ca1e783902
Luis Grasso <luis@carbula.com>
2022-06-15 19:26:14 -0300

    Update

--- pages/creditos-para-autos/index.jsx
+++ pages/creditos-para-autos/index.jsx
@@ -120,7 +120,33 @@ const CreditosAutos = () => {
 
         <div className={styles.creditos}>
           <h3>¿Por qué comprar en nuestra plataforma?</h3>
-          <div class="ui cards"><div class="ui card"><div class="content"><div class="header">Project Report - April</div><div class="meta">ROI: 30%</div><div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - May</div><div class="meta">ROI: 34%</div><div class="description">Bring to the table win-win survival strategies to ensure proactive domination.</div></div></div><div class="ui card"><div class="content"><div class="header">Project Report - June</div><div class="meta">ROI: 27%</div><div class="description">Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.</div></div></div></div>
+          <div class="ui cards"><div class="ui card">
+          <div class="content">
+          <div class="header">
+          <Image src='/images/avatar.png'></Image>
+          </div><div class="header">ROI: 30%</div>
+          <div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div>
+          </div>
+          </div>
+        </div>
+        <div class="ui cards"><div class="ui card">
+          <div class="content">
+          <div class="header">
+          <Image src='/images/avatar.png'></Image>
+          </div><div class="header">ROI: 30%</div>
+          <div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div>
+          </div>
+          </div>
+        </div>
+        <div class="ui cards"><div class="ui card">
+          <div class="content">
+          <div class="header">
+          <Image src='/images/avatar.png'></Image>
+          </div><div class="header">ROI: 30%</div>
+          <div class="description">Leverage agile frameworks to provide a robust synopsis for high level overviews.</div>
+          </div>
+          </div>
+        </div>
 
         </div>
 

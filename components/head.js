4b2d4ead3a447247470fdba51cccfe8bad52459a
Luis Grasso <luis@carbula.com>
2022-06-28 09:35:30 -0300

    Fix data cache-control

--- components/head.js
+++ components/head.js
@@ -17,7 +17,7 @@ const Head = props => {
       <title>{props.title || getTitleByCountry(COUNTRY_CODE)}</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+      <meta http-equiv="Cache-Control" content="max-age: 31536000, no-cache, no-store, must-revalidate" />
       <meta http-equiv="Pragma" content="no-cache" />
       <meta http-equiv="Expires" content="0" />
       <meta name="description" content={props.description || defaultDescription} />

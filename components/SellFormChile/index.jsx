57caa6b50fd191e163a99ad2902e676840cf28c9
Luis Grasso <luis@carbula.com>
2022-07-05 17:44:46 -0300

    Feature/add sellform newsletter checkbox CH

--- components/SellFormChile/index.jsx
+++ components/SellFormChile/index.jsx
@@ -516,17 +516,19 @@ const SellFormChile = ({ step, setStep, setOverlayBackground, zonas, referer, CO
               </div>
             )}
           </div>
-
         </div>
-        <div>
-          <input type="checkbox" id="last" name="last" required />
-          <label className={styles.terms}>Sí, quiero recibir newsletters</label>
-        </div>
-        <div className={styles.buttons__container}>
-          <Button type="button" link onClick={handleBack}>Volver</Button>
-          <Button overlayEffect type="submit" primary>Cotizar</Button>
-        </div>
-        <p className={styles.terms}>Al enviar este formulario, usted acepta los <a href="/terminos-y-condiciones" target="__blank">Términos de Servicio</a> y la <a href="/terminos-y-condiciones" target="__blank">Política de Privacidad de Cárbula</a>.</p>
+        {/* <GoogleOneTapLogin/> */}
+        <div className={styles.sellform__container}>
+          <div className={styles.checkbox}>
+            <input type="checkbox" id="last" name="last" required />
+            <label>Quiero recibir newsletters</label>
+          </div>
+          </div>
+          <div className={styles.buttons__container}>
+            <Button type="button" link onClick={handleBack}>Volver</Button>
+            <Button overlayEffect type="submit" primary>Cotizar</Button>
+          </div>
+          {/*<p className={styles.terms}>Al enviar este formulario, usted acepta los <a href="/terminos-y-condiciones" target="__blank">Términos de Servicio</a> y la <a href="/terminos-y-condiciones" target="__blank">Política de Privacidad de Cárbula</a>.</p>*/}
       </form>
     }
     return formSteps[step]

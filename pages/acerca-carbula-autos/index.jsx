9f5b2b28773b4731ef63418530fe9da6a9911d19
Luis Grasso <luis@carbula.com>
2022-08-16 12:45:44 -0300

    Add sections

--- pages/acerca-carbula-autos/index.jsx
+++ pages/acerca-carbula-autos/index.jsx
@@ -47,12 +47,56 @@ const AcercaCarbula = () => {
       <div className={styles.acerca}>
       
       <Container>
-        <roW>
       <div class="ui vertical stripe segment">
         <div class="ui middle aligned stackable grid container">
           <div class="row">
             <div class="eight wide column">
-              <h3 class="ui header">We Help Companies and Companions</h3>
+              <h3 class="ui header">¿Qué es Cárbula?</h3>
+              <p>
+              Somos un marketplace de compra y venta de autos usados para vender de manera más eficiente y transparente. Generamos un entorno de intermediación, uniendo gente que quiere vender con gente que busca comprar autos usados. 
+              Le permitimos a los dueños vender su auto por hasta un 25% más de dinero sin salir de su casa en 20 días o menos, brindando operaciones seguras, simples y cómodas.              </p>
+            </div>
+            <div class="six wide right floated column">
+              <Image class="ui large image" src="/images/auto-carbula.webp"></Image>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Container>
+
+      <Divider></Divider>
+      
+      <Container>
+      <div class="ui vertical stripe segment">
+        <div class="ui middle aligned stackable grid container">
+          <div class="row">
+          <div class="six wide right floated column">
+              <Image class="ui large image" src="/images/auto-carbula.webp"></Image>
+            </div>
+            <div class="eight wide column">
+              <h3 class="ui header">Misión</h3>
+              <p>
+              Transformar la compra y venta de autos en algo placentero. Buscamos impactar positivamente en la vida de millones de personas que compran y venden su auto de manera diaria perdiendo tiempo, gastando más dinero del que debieran, y desarrollando patrones de estrés y ansiedad durante la transacción.
+              </p>
+              <h3 class="ui header">Visión</h3>
+              <p>
+              Convertirnos en los máximos exponentes en el rubro de compra y venta de vehículos usados y tener presencia en al menos 30 ciudades latinoamericanas en los próximos 5 años. 
+              </p>
+            </div>
+          </div>
+         
+          </div>
+        </div>
+      </Container>  
+
+      <Divider></Divider>   
+
+      <Container>
+      <div class="ui vertical stripe segment">
+        <div class="ui middle aligned stackable grid container">
+          <div class="row">
+            <div class="eight wide column">
+              <h3 class="ui header">Nuestros valores</h3>
               <p>
                 We can give your company superpowers to do things that they
                 never thought possible. Let us delight your customers and
@@ -68,82 +112,37 @@ const AcercaCarbula = () => {
               <Image class="ui large image" src="/images/auto-carbula.webp"></Image>
             </div>
           </div>
+        </div>
+      </div>
+      </Container> 
+
+      
+      <Divider></Divider>   
+
+      <Container>
+      <div class="ui vertical stripe segment">
+        <div class="ui middle aligned stackable grid container">
           <div class="row">
-            <div class="center aligned column">
-              <a class="ui huge button">Check Them Out</a>
+          <div class="six wide right floated column">
+              <Image class="ui large image" src="/images/auto-carbula.webp"></Image>
+            </div>
+            <div class="eight wide column">
+              <h3 class="ui header">Cárbula en la prensa</h3>
+              <p>
+                We can give your company superpowers to do things that they
+                never thought possible. Let us delight your customers and
+                empower your needs...through pure data analytics.
+              </p>
+              <h3 class="ui header">We Make Bananas That Can Dance</h3>
+              <p>
+                Yes that's right, you thought it was the stuff of dreams, but
+                even bananas can be bioengineered.
+              </p>
             </div>
           </div>
         </div>
       </div>
-      </roW>
-      </Container>
-        
-
-     
-
-          <div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
-            <h4 itemprop="name">1. ¿Revisan los autos?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Sí, nuestros inspectores realizan una rigurosa inspección mecánica de más de 150 puntos para asegurarse de que todos los autos estén en excelentes condiciones. Solo trabajamos con autos que aprueben el protocolo de inspección.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">2. ¿Qué tipo de autos tienen?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Contamos con una amplia variedad de modelos y marcas. Todos nuestros vehículos tienen menos de 10 años de antigüedad, poseen menos de 150.000 kilómetros, aprobaron la inspección técnica y <b>están listos para que sean tuyos.</b></p>
-              </div>
-            </div>
-            <h4 itemprop="name">3. Me interesa un vehículo en particular, ¿qué hago?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                  <p>Primero, dentro de la publicación hace click en "ME INTERESA”. Luego, uno de nuestros asesores se contactará y te brindará toda la información necesaria (contamos con los informes mecánicos más completos del mercado).</p>
-                  <p>El segundo paso es agendar una visita.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">4. ¿En qué consiste la visita vehicular?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Es una cita en el día y horario que te quede más conveniente para que conozcas tu futuro auto. Uno de nuestros asesores estará allí para despejar todas tus dudas y acompañarte durante el encuentro.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">5. Todavía no sé qué auto quiero</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>No hay ningún problema. Queremos que tengas la mejor Experiencia posible, por eso nuestros asesores te brindarán asesoramiento personalizado para ayudarte a elegir tu vehículo ideal.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">6. ¿Qué métodos de pago aceptan?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Aceptamos efectivo o transferencia bancaria. Asimismo, siempre buscamos acordar el método de pago en base a lo que le resulte más conveniente tanto a la parte compradora como vendedora.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">7. ¿Brindan financiación?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Sí. Financiamos desde el <b>50% del valor de tu vehículo</b> y trabajamos con los líderes nacionales en prendarios para que puedas acceder a tu nuevo auto de la mejor manera.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">¿Aceptan mi auto cómo método de pago?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Sí, siempre que tu auto cumpla con los requisitos necesarios para formar parte de nuestro catálogo. Le realizaremos una rigurosa inspección mecánica para conocer su estado y definir su valor real.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">9. ¿Cómo es el proceso de compra?</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Simple y transparente. Durante la venta nuestro asesor coordinará el intercambio con el vendedor. Nosotros te acompañaremos durante todo el trámite para que luego de la visita, el auto sea tuyo en los próximos días.</p>
-              </div>
-            </div>
-            <h4 itemprop="name">10. Listo, quiero comprar con ustedes.</h4>
-            <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-              <div itemprop="text">
-                <p>Perfecto. Muchas gracias por confiar en nosotros y te damos oficialmente la bienvenida a la Experiencia Cárbula. Hacé click en nuestro catálogo para conocer todos los vehículos disponibles y nuestros asesores se encargarán del resto.</p>
-              </div>
-            </div>
-          </div>
+      </Container> 
       </div>
       <hr />
       <section>

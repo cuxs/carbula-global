81040879b6c776761f2a95bd3f907efdcab7fabe
JuanBautista2021 <juanbautista@carbula.com>
2023-01-03 12:46:00 -0300

    Skipped autoinspector on desktop

--- components/CotizationForm/index.jsx
+++ components/CotizationForm/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, Fragment, useCallback, useEffect } from 'react';
-import { formatNumber, getCalendlyURL, getCatalogoURL, estadosMX, redondeo, getCotization } from '../../utils/helpers';
+import { formatNumber, getCalendlyURL, getCatalogoURL, estadosMX, redondeo, getCotization, getWhatsappNumber } from '../../utils/helpers';
 import Button from '../Button';
 import styles from './cotization-form.module.scss';
 import Select from '../SelectComponent';
@@ -175,13 +175,14 @@ const CotizationForm = ({
         "color": typeof(carAndContactData.inspectionDataColor) !== "undefined" ? carAndContactData.inspectionDataColor : "white",
         "external_id": carAndContactData.external_id
       }
-      await generateInspectionHS(inspectionData)
-      .then(res => {setInspectionURL(res)})
-      .catch(err => {console.log("ERROR: ", err)})
+      // await generateInspectionHS(inspectionData)
+      // .then(res => {setInspectionURL(res)})
+      // .catch(err => {console.log("ERROR: ", err)})
+      setInspectionURL("No se pudo especificar")
     }
     catch(err){
       console.log("ERROR: ", err)
-      setInspectionId("No se pudo especificar")
+      // setInspectionId("No se pudo especificar")
       setInspectionURL("No se pudo especificar")
     }
   }
@@ -670,14 +671,16 @@ const CotizationForm = ({
   </div>
 
   const Setp4Desktop = () => <div>
-    <p>Puede hacerlo escaneando el código QR de abajo. También le enviaremos el enlace por <b>correo electrónico a {email}</b></p>
-    <QRCode value={inspectionURL} size="128"/>
+    {/* <p>Puede hacerlo escaneando el código QR de abajo. También le enviaremos el enlace por <b>correo electrónico a {email}</b></p>
+    <QRCode value={inspectionURL} size="128"/> */}    
+    <Button><a href={`http://api.whatsapp.com/send?phone=${getWhatsappNumber(COUNTRY_CODE)}&text=Hola,%20soy%20${name}%20y%20quiero%20vender%20mi%20${brand}%20${model}%20${version}%20${year}`}><b>Continuar</b></a></Button>
   </div>
 
   const Setp4Mobile = () => <div>
-    <p>Puede hacerlo presionando el botón de abajo. También le enviaremos el enlace por <b>correo electrónico a {email}</b></p>
+    {/* <p>Puede hacerlo presionando el botón de abajo. También le enviaremos el enlace por <b>correo electrónico a {email}</b></p>
     <br /><br />
-    <Button><a href={inspectionURL}>Inspección virtual</a></Button>
+    <Button><a href={inspectionURL}>Inspección virtual</a></Button> */}
+    <Button><a href={inspectionURL}>Continuar</a></Button>
   </div>
 
   const Step5Mobile = () => <div className={styles['secondary-steps__container']}>

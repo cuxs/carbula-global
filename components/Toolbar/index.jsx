7c21db767ea1f5d0e2dd97c939dd2cf061887c90
luis-carbula <luis@carbula.com>
2024-10-14 12:46:39 -0400

    edit-nav-icons

--- components/Toolbar/index.jsx
+++ components/Toolbar/index.jsx
@@ -6,12 +6,12 @@ import styles from './toolbar.module.scss';
 import classNames from 'classnames/bind';
 import { useSpring } from 'react-spring';
 import { getCatalogoURL, getCountryCode, getWhatsappNumber } from "../../utils/helpers";
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 const Toolbar = ({ drawerClickHandler }) => {
   // const [isTop, setIsTop] = useState('');
   const router = useRouter()
-  const COUNTRY_CODE= getCountryCode(router.locale)
+  const COUNTRY_CODE = getCountryCode(router.locale)
   const [sideMenu, setSideMenu] = useState(false);
 
   const classNameContext = classNames.bind(styles)
@@ -40,13 +40,13 @@ const Toolbar = ({ drawerClickHandler }) => {
       <div className={styles['toolbar__container-items']} style={props}>
         <div><Link href="/">
           <div className={styles.toolbar__logo}>
-            <img src="/images/logo.svg"  alt="Cárbula Autos"/>
+            <img src="/images/logo.svg" alt="Cárbula Autos" />
           </div></Link></div>
         <div className={styles.spacer} />
         <div className={styles.toolbar__items}>
-        {/* <a href={getCatalogoURL(COUNTRY_CODE)}target="__blank"><b>Comprar un auto</b></a> */}
+          {/* <a href={getCatalogoURL(COUNTRY_CODE)}target="__blank"><b>Comprar un auto</b></a> */}
           <a href="/"><b>Vender mi auto</b></a>
-          <a hidden={COUNTRY_CODE==='pe' ? true : false} href={ COUNTRY_CODE==='ar' ? '/creditos-para-autos' : `http://api.whatsapp.com/send?phone=${getWhatsappNumber(COUNTRY_CODE)}&text=Hola,%20necesito%20financiar%20la%20compra%20de%20un%20auto`}><b>Financiar tu compra</b></a>
+          <a hidden={COUNTRY_CODE === 'pe' ? true : false} href={COUNTRY_CODE === 'ar' ? '/creditos-para-autos' : `http://api.whatsapp.com/send?phone=${getWhatsappNumber(COUNTRY_CODE)}&text=Hola,%20necesito%20financiar%20la%20compra%20de%20un%20auto`}><b>Financiar tu compra</b></a>
           <a href="/preguntas-frecuentes"><b>FAQs</b></a>
           <a href={`http://api.whatsapp.com/send?phone=${getWhatsappNumber(COUNTRY_CODE)}&text=Hola,%20tengo%20una%20consulta`} target="__blank"><b>Contacto</b></a>
           {/* <Button><a href={getCatalogoURL(COUNTRY_CODE)} target="__blank">Comprar un auto</a></Button> */}
@@ -56,8 +56,12 @@ const Toolbar = ({ drawerClickHandler }) => {
         <DrawerToggleButton click={drawerClickHandler} />
       </div>
       <div className={styles.icons_nav}>
-        <img src="/icons/instagram.svg" alt="" className={styles.icon_insta} />
-        <img src="/icons/whatsapp-white.svg" alt="" className={styles.icon_wsp}/>
+        <a href="https://www.instagram.com/carbula.chile/" target="_blank" rel="noopener noreferrer">
+          <img src="/icons/instagram.svg" alt="Instagram" className={styles.icon_insta} />
+        </a>
+        <a href="`http://api.whatsapp.com/send?phone=${getWhatsappNumber(COUNTRY_CODE)}&text=Hola,%20tengo%20una%20consulta`" target="_blank" rel="noopener noreferrer">
+          <img src="/icons/whatsapp-white.svg" alt="WhatsApp" className={styles.icon_wsp} />
+        </a>
       </div>
     </header>
   )
